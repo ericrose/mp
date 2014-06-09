31e839fe544a8e87c59905bcc042a481d482aaae
ericrose <eric.rose@gmail.com>
2014-06-08 23:07:42 -0400

    Updates to data/pagination
    to fix pagination

--- static/js/mp.js
+++ static/js/mp.js
@@ -2,13 +2,15 @@
 * mp.js v0.0.1 by @an_eric
 * Copyright 2013 Eric Rose
 * http://www.apache.org/licenses/LICENSE-2.0
-*/     
+*/
      var nycm = new Date("Sept. 28, 2014");
 
       $(document).ready(function() {
 
-      $.getJSON( "data/5518.json", function( data ) 
-      {  
+      var maxPage=18;
+
+      $.getJSON( "data/5518.json", function( data )
+      {
           var tempDate = new Date(nycm.getTime());
           var today = new Date();
 
@@ -17,9 +19,9 @@
               tempDate = new Date(nycm.getTime());
               tempDate.setDate(nycm.getDate() - ((parseInt(week.weekNumber) * 7)- (1+parseInt(key))));
 
-              $('#trainingWeek').append('<tr class="' + week.weekNumber +'">'+ 
+              $('#trainingWeek').append('<tr class="' + week.weekNumber +'">'+
                 '<td><b>'+ day.day +
-                '</b></td><td>'+ (1 + tempDate.getMonth()) + "/" + tempDate.getDate() + "/" + tempDate.getFullYear() + 
+                '</b></td><td>'+ (1 + tempDate.getMonth()) + "/" + tempDate.getDate() + "/" + tempDate.getFullYear() +
                 '</td><td>' +day.description +
                 '</td><td>'+ day.distance +
                 '</td></tr>');
@@ -40,10 +42,26 @@
       function buildLists(id, start, end, openTag, direction){
         var html = openTag;
         for(var i=start; (direction) * i < end; i+=direction){
-          html+= '<li><a href="#"' + i+ '">' + i +'</a></li>';
+          html+= '<li><a href="#" id="'+i+'">' + i +'</a></li>';
         }
         html+='</ul>';
         $(id).append(html);
+      }
+
+      function changePage(pageNumber){
+/*        if(parseInt(pageNumber) == 1){
+          $('.next').addClass('disabled');
+        } else {
+          $('.next').removeClass('disabled');
+          TODO:
+          [] - fix pagination :(
+        }*/
+        var targetPage = $('.navbar-nav li[id='+pageNumber+']');
+        $(targetPage).parents('.navbar-nav').find('.active').removeClass('active');
+        $(targetPage).addClass('active');
+        var rows = $('table#trainingWeek tr');
+        var trainingPage = rows.filter('.'+ pageNumber).show();
+        rows.not( trainingPage ).hide();
       }
 
       $('.dropdown-menu li').click(function(e){
@@ -52,8 +70,13 @@
         $(this).parents('.input-group').find('.goalTime').val(selected);
       });
       $('.navbar-nav li').click(function(e){
-        $(this).parents('.navbar-nav').find('.active').removeClass('active');
-        $(this).addClass('active');
+        changePage($(this).text());
+      });
+      $('.next').click(function(e){
+        changePage(parseInt($('.navbar-nav').find('.active').text())-1);
+      });
+      $('.previous').click(function(e){
+        changePage(parseInt($('.navbar-nav').find('.active').text())+1);
       });
       $("#generate").click(function() {
         $.post( ".", $("#targetsForm").serialize(),
@@ -73,8 +96,8 @@
               .always(function() {
                 $("#targetsModal").modal('hide');
               });
-      })
-    })
+      });
+    });
     $(function() {
       $( "#datepicker" ).datepicker();
     });
