6be13f88ee8226622cc2a3fe507dcfb0a897a123
Eric <eric.rose@gmail.com>
2014-06-11 15:38:11 -0400

    updates to pagination, buildout of plan

--- static/js/mp.js
+++ static/js/mp.js
@@ -3,22 +3,34 @@
 * Copyright 2013 Eric Rose
 * http://www.apache.org/licenses/LICENSE-2.0
 */
-     var nycm = new Date("Oct. 5, 2014");
+var nycm = new Date("Oct. 5, 2014");
 
-      $(document).ready(function() {
+$(document).ready(function() {
 
-      var maxPage = 18;
+  var paceCookie = document.cookie;
 
-      $.getJSON( "data/5518.json", function( data )
+  if(document.cookie)
+
+
+      $('#targetsModal').modal('show');
+
+      $.ajax({
+      url: "data/5518.json",
+      async: false,
+      dataType: 'json',
+      success: function( data )
       {
           var tempDate = new Date(nycm.getTime());
-          var today = new Date();
-          maxPage = data.weeks.length;
 
           $.each(data.weeks, function(weekIndex, week){
+            $('#weekTitle').append('<div id="title'+week.weekNumber+'"><h1 id="mesocycle'+week.weekNumber+'">Mesocycle '+week.phase+':</h1> ' +
+              '<h2 id="description'+week.weekNumber+'">'+week.description+'</h2></div>');
+
             $.each(week.days, function(key, day){
               tempDate = new Date(nycm.getTime());
-              tempDate.setDate(nycm.getDate() - ((parseInt(week.weekNumber) * 7)- (1+parseInt(key))));
+
+              //weeks and days are zero indexed
+              tempDate.setDate(nycm.getDate() - (((parseInt(week.weekNumber) + 1) * 7) - (1 + parseInt(key))));
 
               $('#trainingWeek').append('<tr class="' + week.weekNumber +'">'+
                 '<td><b>'+ day.day +
@@ -28,16 +40,24 @@
                 '</td></tr>');
             });
           });
-          var todayRow = $( "td:contains('"+ (1 + today.getMonth()) + "/" + today.getDate() + "/" + today.getFullYear() +"')");
-          $(todayRow).parent().addClass("success");
-          changePage($(todayRow).parent().attr("class").split(" ")[0]);
-          /*FIXME: goto today*/
+
+          initPage(data.weeks.length);
+        }  
       });
+      
+      function initPage(totalWeeks){
 
 
-      buildLists('#weekNav', maxPage, 0, '<ul class="nav navbar-nav">', -1);
-      buildLists('#minutesList', 0, 60, '<ul class="dropdown-menu" id="minutes">', 1);
-      buildLists('#hoursList', 2, 6, '<ul class="dropdown-menu minutes" id="hours">', 1);
+        var today = new Date();
+        var todayRow = $( "td:contains('"+ (1 + today.getMonth()) + "/" + today.getDate() + "/" + today.getFullYear() +"')");
+        $(todayRow).parent().addClass("success");
+        buildLists('#weekNav', totalWeeks -1, 1, '<ul class="nav navbar-nav">', -1);
+        //what if training hasn't started yet???
+        changePage($(todayRow).parent().attr("class").split(" ")[0]);      
+        
+        buildLists('#minutesList', 0, 60, '<ul class="dropdown-menu" id="minutes">', 1);
+        buildLists('#hoursList', 2, 6, '<ul class="dropdown-menu minutes" id="hours">', 1);
+      }
 
       function buildLists(id, start, end, openTag, direction){
         var html = openTag;
@@ -49,16 +69,20 @@
       }
 
       function changePage(pageNumber){
-        if(parseInt(pageNumber) == 1){
-          $('.next').addClass('disabled');
-        } else {
-          $('.next').removeClass('disabled');
-        }
+        var intPage = parseInt(pageNumber);
+        (intPage==0) ? $('.next').hide() : $('.next').show();
+        (intPage==parseInt($('.navbar-nav li').eq(0).text())) ? $('.previous').hide() : $('.previous').show();
+
         var targetPage = $('.navbar-nav li').filter(function(){return $(this).text()==pageNumber;});
         $(targetPage).parents('.navbar-nav').find('.active').removeClass('active');
         $(targetPage).addClass('active');
         var rows = $('table#trainingWeek tr');
+        var titles = $('#weekTitle div');
+
+        var trainingTitle = $('#weekTitle div[id="title'+pageNumber+'"]').show();
         var trainingPage = rows.filter('.'+ pageNumber).show();
+
+        titles.not( trainingTitle ).hide();
         rows.not( trainingPage ).hide();
       }
 
@@ -71,9 +95,11 @@
         changePage($(this).text());
       });
       $('.next').click(function(e){
+        e.preventDefault();
         changePage(parseInt($('.navbar-nav').find('.active').text())-1);
       });
       $('.previous').click(function(e){
+        e.preventDefault();
         changePage(parseInt($('.navbar-nav').find('.active').text())+1);
       });
       $("#generate").click(function() {
